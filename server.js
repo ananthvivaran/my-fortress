1cdce96cc876c51866efcc3c15645ada64ad4edb
ananthvivaran <128668295+ananthvivaran@users.noreply.github.com>
2024-05-12 18:21:53 +0530

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const port = 3000;
 
 app.use(express.json());
 app.use(cors({
-  origin: 'https://ananthvivaran.github.io/my-fortress/'
+  origin: '*'
 }));
 
 const url = 'mongodb://localhost:27017';
